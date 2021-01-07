c5bc949bf6d1d7fa58a900c819d210f91faec1e3
alkim civelek <alkimcivelek@outlook.com>
2021-01-07 14:38:10 +0300

    Console App

---
+++ CarGallery.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CarGallery.Business.Abstract;
+using CarGallery.Business.CrossCuttingConcerns.DependencyResolvers.Ninject;
+using System;
+
+namespace CarGallery.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var carService = InstanceFactory.Get<ICarService>();
+            var list = carService.GetAll();
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
