ddb18e9cb73bd4f369759dbbb49700ad7a6e6429
alkim civelek <alkimcivelek@outlook.com>
2021-01-07 14:30:25 +0300

    IOC implementation(Ninject)

---
+++ CarGallery.Business/CrossCuttingConcerns/DependencyResolvers/Ninject/BusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using CarGallery.Business.Abstract;
+using CarGallery.Business.Concrete;
+using CarGallery.DataAccess.Abstract;
+using CarGallery.DataAccess.Concrete.EntityFramework;
+using Ninject.Modules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarGallery.Business.CrossCuttingConcerns.DependencyResolvers.Ninject
+{
+    public class BusinessModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<ICarService>().To<CarManager>().InSingletonScope();
+            Bind<ICarDal>().To<EFCarDal>().InSingletonScope();
+        }
+    }
+}
