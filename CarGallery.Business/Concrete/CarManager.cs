c0e8d7648a283bbbb7ecbc9eeb4554f949fda322
alkim civelek <alkimcivelek@outlook.com>
2021-01-07 14:10:35 +0300

    Business Logic Layer has been implemented

---
+++ CarGallery.Business/Concrete/CarManager.cs
@@ -0,0 +1,45 @@
+﻿using CarGallery.Business.Abstract;
+using CarGallery.DataAccess.Abstract;
+using CarGallery.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace CarGallery.Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        private ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public void Add(Car entity)
+        {
+            _carDal.Add(entity);
+        }
+
+        public void Delete(Car entity)
+        {
+            _carDal.Delete(entity);
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            return _carDal.Get(filter);
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            return _carDal.GetAll();
+        }
+
+        public void Update(Car entity)
+        {
+            _carDal.Update(entity);
+        }
+    }
+}
